55e6a2aa541493974fd0bb50eae41f05852abc93
juju4 <juju4@users.noreply.github.com>
2019-02-02 15:30:10 -0500

    galaxy lint, dependency naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,10 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/Mayeu.RabbitMQ ] && git clone https://github.com/juju4/ansible-playbook-rabbitmq.git $rolesdir/Mayeu.RabbitMQ
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.mig-rabbitmq ] && ln -s ansible-mig-rabbitmq $rolesdir/juju4.mig-rabbitmq
 [ ! -e $rolesdir/juju4.mig-rabbitmq ] && cp -R $rolesdir/ansible-mig-rabbitmq $rolesdir/juju4.mig-rabbitmq
 
 ## don't stop build on this script return code
 true
-
